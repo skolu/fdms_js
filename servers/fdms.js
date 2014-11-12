7d7c9f50962223c8511a0de249230b90da5e10af
Sergey Kolupaev <skolupaev@gmail.com>
2014-11-11 22:18:43 -0800

    Nov 11

--- servers/fdms.js
+++ servers/fdms.js
@@ -1,11 +1,12 @@
 var constants = require('./constants');
 var buf = require('./buffer');
+var txn = require('./fdms_txn');
 var EventEmitter = require('events').EventEmitter;
 
 var FdmsSession = function () {
   this.fdms_connection = null;
   this.fdms_stream = new FdmsStream();
-  this.fdms_stream.on('packet', this.onFdmsPacket.bind(this));
+  this.attempt = 0;
 };
 
 FdmsSession.prototype.listener = function (stream) {
@@ -14,26 +15,115 @@ FdmsSession.prototype.listener = function (stream) {
     this.fdms_stream.append(chunk);
   }.bind(this));
   stream.once('finish', this.onFinish.bind(this));
-  this.write(constants.ENQ);
+
+  this.attempt = 0;
+  this.write(constants.ENQ, this.read(this.read_request));
 };
 
-FdmsSession.prototype.write = function (data) {
+FdmsSession.prototype.read_request = function(packet) {
+  if (this.attempt > 3) {
+    this.fdms_connection.end();
+    return;
+  }
+  var controlByte = packet[0];
+  if (controlByte === constants.STX) {
+    try {
+      var txn = this.parse(packet);
+      if (txn.header.txn_type === '0') {
+        this.write(constants.ACK, this.read(this.read_request));
+      }
+      this.attempt = 0;
+    }
+    catch(err) {
+      console.log(err);
+      this.attempt ++;
+      this.write(constants.NAK, this.read(this.read_request));
+    }
+  } else if (controlByte === constants.EOT) {
+    this.process();
+  } else {
+    this.fdms_connection.end();
+  }
+}
+
+FdmsSession.prototype.parse = function (data) {
+  var lrs = packet[1];
+  for (var i = 2; i < packet.length - 2; i++) {
+    lrs ^= packet[i];
+  }
+  if (lrs !=== packet[packet.length - 1]) {
+    throw ("Invalid LRS");
+  }
+  var header = new FdmsHeader();
+  var pos = 0
+
+  if (data[pos] === constants.STX)
+    pos += 1
+
+  var p_ch = data.toString('ascii', pos, pos + 1);
+  if (p_ch !== '*')
+    throw "Protocol flag * expected";
+
+  pos += 1
+  header.protocol_type = data.toString('ascii', pos, pos + 1);
+
+  pos += 1
+  this.terminal_id = data.toString('ascii', pos, pos + 5);
+
+  pos += 5
+    p_sep = data.index(SEP, pos, pos + 20)
+    merch_num = data[pos:p_sep].decode()
+
+    pos = p_sep + 1
+    p_sep = data.index(FS, pos, pos+5)
+    device_id = data[pos:p_sep].decode()
+
+    pos = p_sep + 1
+    wcc = data[pos:pos+1].decode()
+
+    pos += 1
+    txn_type = data[pos:pos+1].decode()
+
+    pos += 1
+    txn_code = data[pos:pos+1].decode()
+
+    pos += 1
+    p_sep = data[pos]
+    if p_sep != FS:
+        raise ValueError('Invalid transaction header')
+
+    pos += 1
+
+};
+
+FdmsSession.prototype.write = function (data, func) {
   var packet = data;
   if (!Buffer.isBuffer(data)) {
     packet = new Buffer(1);
     packet[0] = data;
   }
   var ok = this.fdms_connection.write(packet);
-  if (!ok) {
-    this.fdms_connection.once('drain', function() {
-      this.fdms_connection.write(packet);
-    });
+  if (ok) {
+    func();
+    return;
   }
+  var callback = function() {
+    this.fdms_connection.write(packet);
+    func();
+  }.bind(this);
+  this.fdms_connection.once('drain', callback);
 };
 
-FdmsSession.prototype.onFdmsPacket = function (packet) {
-  console.log("FDMS packet received");
-  this.fdms_connection.end();
+FdmsSession.prototype.read = function (func) {
+  var packet = this.fdms_stream.next();
+  if (packet) {
+    funcData(packet);
+  } else {
+    var callback = function () {
+      funcData(packet);
+    }.bind(this);
+    this.fdms_stream.once('available', callback);
+  }
 };
 
 FdmsSession.prototype.onFinish = function () {
@@ -44,40 +134,61 @@ FdmsSession.prototype.onFinish = function () {
 
 var FdmsStream = function() {
   this.buffer = new buf.ReusableBuffer();
+  this.next_packet = null;
 };
 
 FdmsStream.prototype = new EventEmitter();
 FdmsStream.prototype.append = function (chunk) {
   this.buffer.append(chunk);
-  while (this.buffer.size() > 0) {
-    var b = this.buffer.byteAt(0);
-    switch (b) {
-      case constants.STX:
-      var packet_end = 0;
-      for (var i = 1; i < this.buffer.size(); i++) {
-        if (this.buffer.byteAt(i) === constants.ETX) {
-          if (i+1 < this.buffer.size()) {
-            packet_end = i + 1;
-            break;
+  if (!this.next_packet) {
+    this.shift();
+  }
+  if (this.next_packet) {
+    this.emit('available');
+  }
+};
+
+FdmsStream.prototype.shift = function () {
+  if (!this.next_packet) {
+    while (this.buffer.size() > 0) {
+      var b = this.buffer.byteAt(0);
+      switch (b) {
+        case constants.STX:
+        var packet_end = 0;
+        for (var i = 1; i < this.buffer.size(); i++) {
+          if (this.buffer.byteAt(i) === constants.ETX) {
+            if (i+1 < this.buffer.size()) {
+              packet_end = i + 1;
+              break;
+            }
           }
         }
-      }
-      if (packet_end === 0) {
+        if (packet_end === 0) {
+          return;
+        }
+        this.next_packet = this.buffer.cut(packet_end);
         return;
+
+        case constants.ACK:
+        case constants.NAK:
+        case constants.EOT:
+        case constants.ENQ:
+        this.next_packet =  this.buffer.cut(1);
+        return;
+
+        default:
+        this.buffer.shift(1);
+        console.log("Unexpected char in FDMS stream");
+        break;
       }
-      this.emit('packet', this.buffer.cut(packet_end));
-      break;
-      case constants.ACK:
-      case constants.NAK:
-      case constants.EOT:
-      case constants.ENQ:
-      this.emit('packet', this.buffer.cut(1));
-      break;
-      default:
-      this.buffer.shift(1);
-      console.log("Unexpected char in FDMS stream");
     }
   }
+};
+
+FdmsStream.prototype.next = function () {
+  var p = this.next_packet;
+  this.shift();
+  return p;
 };
 
 module.exports.FdmsSession = FdmsSession;
---
+++ servers/fdms_txn.js
@@ -0,0 +1,12 @@
+var FdmsHeader = function() {
+        this.protocol_type = ''
+        this.terminal_id = ''
+        self.merchant_number = ''
+        self.device_id = ''
+        self.wcc = ''
+        self.txn_type = ''
+        self.txn_code = '';
+
+}
+
+module.exports.FdmsHeader = FdmsHeader;
